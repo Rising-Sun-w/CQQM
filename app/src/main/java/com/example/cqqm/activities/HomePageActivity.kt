f93b0dfb1a012d2c0fc60ac504090ee4eefdd02f
W <2803724412@qq.com>
2021-12-31 11:37:52 +0800

    first commit

---
+++ app/src/main/java/com/example/cqqm/activities/HomePageActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cqqm.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cqqm.R
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_home_page)
+
+    }
+}
