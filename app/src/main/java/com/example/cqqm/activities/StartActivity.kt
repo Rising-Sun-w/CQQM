6a109d306f8386ba5f9bda5ac66553e0fcf4dafb
W <2803724412@qq.com>
2022-01-02 19:17:45 +0800

    third commit

--- app/src/main/java/com/example/cqqm/activities/StartActivity.kt
+++ app/src/main/java/com/example/cqqm/activities/StartActivity.kt
@@ -5,24 +5,15 @@ import android.os.Bundle
 import com.example.cqqm.R
 import java.lang.Thread.sleep
 import android.content.Intent
+import android.os.Handler
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        drawStop()
-        //Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-        //startActivity(intent);
-//        val intent = Intent(StartActivity.this,HomePageActivity.class)
-        val intent = Intent(this, HomePageActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun drawStop() {
-        Thread(Runnable {
-            kotlin.run {
-                sleep(4 * 10000)
-            }
-        })
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this,HomePageActivity::class.java)
+            startActivity(intent)
+        },1500)
     }
 }
